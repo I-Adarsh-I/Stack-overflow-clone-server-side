ff35737e064f883bd7292dd6de7e5abd16c1471e
@darsh_singh <76638926+I-Adarsh-I@users.noreply.github.com>
2022-12-19 22:19:31 +0530

    back-end

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,20 +11,20 @@ export const signup = async(req,res) => {
             return res.status(404).json({message:"User already exist"});
         }
 
-        const hashedPassword = await bcrypt.hash(password,12);
-        const newUser = await users.create({ name, email, hashedPassword})
-        const token = jwt.sign({email: newUser.email,id: newUser._id}, "test", {expiresIn:"1h"})
-        res.status(200).json({message: newUser, token})
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const newUser = await users.create({ name, email, password: hashedPassword})
+        const token = jwt.sign({email: newUser.email, id: newUser._id}, "test", {expiresIn:"1h"})
+        res.status(200).json({result: newUser, token})
     }
     catch(error){
-        res.status(500).json({message:"Something went wrong..."})
+        res.status(500).json("Something went wrong...")
     }
 } 
 
 export const login = async(req,res) => {
-    const { email,password } = req.body;
+    const { email, password } = req.body;
     try{
-        const existinguser = await users.findOne({email})
+        const existinguser = await users.findOne({email});
         if(!existinguser){
             return res.status(404).json({message:"user do not exist"})
         }
@@ -32,11 +32,11 @@ export const login = async(req,res) => {
         if(!isPasswordCorrect){
             return res.status(400).json({message:"Invalid credentials"})
         }
-        const token = jwt.sign({email: newUser.email,id: newUser._id}, "test", {expiresIn:"1h"})
-        res.status(200).json({message: newUser, token})
+        const token = jwt.sign({email: existinguser.email, id:existinguser._id}, "test", {expiresIn:"1h"})
+        res.status(200).json({result: existinguser, token})
         
     }catch(error){
-        res.status(500).json({message:"Something went wrong..."})
+        res.status(500).json("Something went wrong...")
 
     }
 
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import cors from 'cors';
+import cors from 'cors'
+import dotenv from 'dotenv';
 
-import userRoutes from './routes/users.js';
+import userRoutes from './routes/users.js'
 
 const app = express();
-
-app.use(express.json({limit:"30mb", extended: true}))
-app.use(express.urlencoded({limit:"30mb", extended: true}))
-app.use(cors())
-
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+dotenv.config();
 app.get('/', (req, res) => {
-    res.send("This is the stackOverflow clone API")
+    res.send("This is a Stack Overflow clone API")
 })
 app.use('/user', userRoutes)
 
-const PORT = process.env.PORT||5000
+const PORT = process.env.PORT || 5000;
 
 const CONNECTION_URL = "mongodb+srv://admin:admin@stack-overflow-clone.scc6wlg.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
